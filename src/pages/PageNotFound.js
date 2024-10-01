c01f81cb4a3a93e9a754a36f6f63638313dc4af2
Zavierzs <zslee0406@gmail.com>
2024-10-01 16:14:53 +0800

    Edited PageNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -4,7 +4,7 @@ import { Button } from "../components";
 import { useTitle } from "../hooks/useTitle";
 
 export const PageNotFound = () => {
-    const pageTitle = useTitle(`Page Not Found`);
+    useTitle(`Page Not Found`);
 
     return (
     <main>
